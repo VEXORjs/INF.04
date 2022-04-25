c8678fc9064f9d6c7d7c578b3bd54810efab0953
VEXORteCHc <kacperadam10@gmail.com>
2022-04-25 20:37:36 +0200

    Create while.js
    Simple while syntax.

---
+++ js/secondChapter/loop/while.js
@@ -0,0 +1,9 @@
+let number_1 = Number(prompt("podaj jakas liczbe: "));
+let number_2 = Number(prompt("podaj jakas liczbe: "));
+
+let sum = number_1 + number_2;
+
+while (sum < 10) {
+    console.log("suma jest mniejsza od 10");
+}
+console.log(sum);
